516f9f18bdc5f9979a2ad18cc52838d49aa5b3f6
Phong Le <phong.le@opswat.com>
2024-04-09 16:19:13 +0700

    add socket.io handling

--- server/api/src/app.js
+++ server/api/src/app.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const compression = require("compression");
 const express = require("express");
+const getDeviceStatus = require('./src/controllers/apartmentController');
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const http = require("http"); // Import the 'http' module for creating an HTTP server
-const WebSocket = require("ws");
+const socketIo = require('socket.io');
 // const apiKey = require("./routes/Auth/checkAuth")
 
 const app = express();
 const server = http.createServer(app); // Create an HTTP server
-const wss = new WebSocket.Server({ server }); // Create a WebSocket server
+const io = socketIo(server); // Create a WebSocket server
 // init middlewares
 
 app.use(morgan("dev")); // print request logs on console
@@ -21,6 +22,40 @@ app.use(bodyParser.urlencoded({
     extended: true  // Sửa từ extends thành extended
 }));
 app.use(bodyParser.json()); // parse application/json
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+  });
+
+
+
+io.on('connection', (socket) => {
+    socket.on('message', async (message) => {
+      let data;
+      try {
+        data = JSON.parse(message);
+      } catch (error) {
+        console.log(`[${new Date().toISOString()}] Invalid JSON: ${message}`);
+        data = {};
+      }
+  
+      try {
+        const document = await getDeviceStatus(data);
+        console.log(document);
+        // Send the 'room' field back to the client
+        if (document) {
+          socket.emit('message', JSON.stringify(document));
+        }
+      } catch (err) {
+        console.error(`[${new Date().toISOString()}] Error handling message: ${err}`);
+      }
+    socket.on('close', () => {
+        console.log('Client disconnected');
+      });
+    });
+  });
+  
 
 // init MongoDb
 require("./db/init_mongo");
