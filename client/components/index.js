fcca89d63133e2b5e62cd1edbf83fbc814418764
Phong Le <phong.le@opswat.com>
2024-05-15 15:47:53 +0700

    remove client frontend folder

--- client/components/index.js
+++
@@ -1,9 +0,0 @@
-import ReusableText from "./Reusable/ReusableText";
-import ReusableButton from "./Reusable/ReusableButton";
-import HeightSpace from "./Reusable/HeightSpace";
-import WidthSpace from "./Reusable/WidthSpace";
-export { 
-    ReusableButton,
-    HeightSpace,
-    WidthSpace,
-    ReusableText }
